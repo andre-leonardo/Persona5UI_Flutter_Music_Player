9a49aaa7fa130f4eb58c2b71c8abe6f153906b6f
André Leonardo <andrevargasgimenes1@gmail.com>
2023-01-29 18:09:54 -0300

    crooked song image square

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -32,6 +32,71 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
+
+
+
+
+
+class ImperfectRectangleBorder extends CustomPainter {
+  final Color strokeColor;
+  ImperfectRectangleBorder({this.strokeColor = Colors.white});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = strokeColor
+      ..strokeWidth = 4.0
+      ..style = PaintingStyle.stroke;
+
+    var path = Path();
+    // Your code to draw the imperfect rectangle
+    path.moveTo(0, -5);
+    path.lineTo(size.width*(0.2), size.height+10);
+    path.lineTo(size.width, size.height-5);
+    path.lineTo(size.height*(0.8), -1 );
+    path.close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(ImperfectRectangleBorder oldDelegate) =>
+      oldDelegate.strokeColor != strokeColor;
+}
+
+class ArtworkWithShape extends StatelessWidget {
+  final Path path;
+  final int id;
+  final ArtworkType type;
+
+  const ArtworkWithShape({required this.path, required this.id, required this.type});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: ShapeClipper(path: path),
+      child: QueryArtworkWidget(
+        artworkBorder: BorderRadius.zero,
+        id: id,
+        type: type,
+      ),
+    );
+  }
+}
+
+class ShapeClipper extends CustomClipper<Path> {
+  final Path path;
+
+  ShapeClipper({required this.path});
+
+  @override
+  Path getClip(Size size) {
+    return path;
+  }
+
+  @override
+  bool shouldReclip(ShapeClipper oldClipper) => oldClipper.path != path;
+}
+
 class _HomeScreenState extends State<HomeScreen> {
 
   final OnAudioQuery _audioQuery = OnAudioQuery();
@@ -69,9 +134,10 @@ class _HomeScreenState extends State<HomeScreen> {
               itemBuilder: ((context, index) {
                 return Container(
                   margin: const EdgeInsets.only(top: 5, left: 12, right: 15),
-                  padding: const EdgeInsets.only(top: 5, bottom: 15),
-                  decoration: const BoxDecoration(
-                    color: Colors.black
+                  padding: const EdgeInsets.only(top: 5, bottom: 5),
+                  decoration:  BoxDecoration(
+                    color: Colors.black,
+                    border: Border.all(color: Colors.grey),
                   ),
 
                   child: ListTile(
@@ -85,7 +151,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           overflow: TextOverflow.ellipsis,
                           maxLines: 1,
                       ),
-                      subtitle: Text(item.data![index].displayName,
+                      subtitle: Text(item.data![index].artist??"<Unknown>",
                           style: const TextStyle(
                               color: Color(0xffffffff),
                               fontFamily: 'Arsenal',
@@ -96,10 +162,19 @@ class _HomeScreenState extends State<HomeScreen> {
                           maxLines: 1,
                       ),
                       trailing: const Icon(Icons.more_vert),
-                      leading: QueryArtworkWidget(
+                      leading: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: CustomPaint(
+                        painter: ImperfectRectangleBorder(strokeColor: Colors.white),
+                        child: QueryArtworkWidget(//from on_audio_query package
+                          artworkBorder: BorderRadius.zero,
+                          artworkFit: BoxFit.fill,
                           id: item.data![index].id,
                           type: ArtworkType.AUDIO,
-                      )
+                        ),
+                      ),
+                    ),
                     ),
 
                 );
@@ -111,6 +186,7 @@ class _HomeScreenState extends State<HomeScreen> {
       );
     
   }
+  
   
   void requestStoragePermission() async {
     //only if the platform is not web, coz web have no permissions
@@ -208,39 +284,3 @@ class _CustomAppBar extends StatelessWidget with PreferredSizeWidget {
 }
 
 
-// class _CustomAppBar extends StatelessWidget with PreferredSizeWidget {
-//   const _CustomAppBar({
-//     Key? key,
-//   }) : super(key: key);
-
-//   @override 
-//   Widget build(BuildContext context) {
-//     return AppBar(
-//       backgroundColor: Colors.transparent,
-//       elevation: 0,
-//       leading: SizedBox(
-//         width: 80,
-//         height: 80,
-//         child: Image.asset("assets/icons/logo.png", height: 78, width: 78,),
-//         ),
-//       actions: [
-//         Container(
-//           margin: const EdgeInsets.only(right: 10),
-//           child: InkWell(
-//             onTap: () {
-//               Navigator.push(
-//                 context, 
-//                 MaterialPageRoute(builder: (context) => const SearchScreen()),
-//               );
-//             },
-//             child: const Image(
-//               image: AssetImage("assets/icons/search.png"),
-//             ),
-//           ),
-//         )
-//       ],
-//     );
-//   }
-//   @override
-//   Size get preferredSize => const Size.fromHeight(100);
-// }
